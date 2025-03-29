095e92c8597bdf856a51f9c665f1647cc1426885
ittiphol.p <ittiphol.puangchaiya@gmail.com>
2025-03-29 11:31:46 +0700

    อัพเดทภาพ

--- components/NavbarHome.jsx
+++ components/NavbarHome.jsx
@@ -37,7 +37,7 @@ const NavbarHome = ({isDarkMode, setIsDarkMode}) => {
       <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 
         ${isScroll ? " bg-transparent backdrop-blur-lg shadow-sm" : ""}`}>
         <a href='#top'>
-          <Image src={isDarkMode ? "/image/ittiphol_dark1.png" : "/image/ittiphol_white1.png"} width={300} height={300} 
+          <Image src={isDarkMode ? "/image/ittiphol-w.png" : "/image/ittiphol-d.png"} width={150} height={150} 
           className='w-28 cursor-pointer mr-14  rounded-full' alt=''/>
         </a>
 
